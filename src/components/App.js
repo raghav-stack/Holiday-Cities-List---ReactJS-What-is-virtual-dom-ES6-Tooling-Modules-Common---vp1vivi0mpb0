b63f6336d586d20bebd225e36772d9a6be037e55
Raghav <kumarraghav71950@gmail.com>
2023-02-24 22:11:48 +0530

    adding solutin

--- src/components/App.js
+++ src/components/App.js
@@ -44,11 +44,13 @@ class App extends Component {
     let j = 1;
     for (let i = 0; i < this.cityList.length; i++) {
       if (this.cityList[i].country === "India") {
-        a.push(<li key={"location" + j}>{this.cityList[i].name}</li>);
+        a.push(<li key={"location" + j++}>{this.cityList[i].name}</li>);
         j++;
       }
     }
+    return a;
   }
+
   render() {
     return <ol>{this.valid()}</ol>;
   }
